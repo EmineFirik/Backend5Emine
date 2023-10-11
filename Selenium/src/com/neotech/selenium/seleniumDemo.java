6df94986c2044dff8493f5a9fea6f009caae793c
EmineFirik <136990724+EmineFirik@users.noreply.github.com>
2023-10-11 18:39:04 -0400

    lesson03

---
+++ Selenium/src/com/neotech/selenium/seleniumDemo.java
@@ -0,0 +1,13 @@
+package com.neotech.selenium;
+
+
+public class seleniumDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.neotechacademy.com");
+	}
+
+}
---
+++ SeleniumRewiev5/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author eminefirik
+ *
+ */
+module SeleniumRewiev5 {
+}
